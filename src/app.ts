d72b3c273c8d93bc0420630a23210272e4966ed6
guilhermesegovia <guilhermemachadosegovia@gmail.com>
2026-04-29 20:44:18 -0300

    Adicionado o README e Arquivos do Controller

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
-import express from "express"
-
-
+import express from "express";
+import { AvaliacaoController } from "./controller/AvaliacaoController";
+import { CarrinhoController } from "./controller/CarrinhoController";
+import { CategoriaController } from "./controller/CategoriaController";
+import { ClienteController } from "./controller/ClienteController";
+import { EnderecoController } from "./controller/EnderecoController";
+import { EstoqueController } from "./controller/EstoqueController";
+import { FuncionarioController } from "./controller/FuncionarioController";
+import { PagamentoController } from "./controller/PagamentoController";
+import { PedidoController } from "./controller/PedidoController";
+import { ProdutoController } from "./controller/ProdutoController";
 
 export const app = express();
 
 app.use(express.json());
+
+AvaliacaoController();
+CarrinhoController();
+CategoriaController();
+ClienteController();
+EnderecoController();
+EstoqueController();
+FuncionarioController();
+PagamentoController();
+PedidoController();
+ProdutoController();
 
 
 app.listen(3000, () => {
